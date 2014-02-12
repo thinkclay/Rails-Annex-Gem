bc97e9395f77b7963da28bf09d423bfa040e6716
Clay McIlrath <clay.mcilrath@gmail.com>
2014-02-11 19:13:00 -0500

    condensing files for auto inclusion and version bump

---
+++ app/assets/javascripts/annex.js
@@ -0,0 +1,5 @@
+//= require annex/cms
+//= require annex/jquery.redactor
+//= require annex/jquery.redactor.save
+//= require annex/jquery.redactor.clips
+//= require annex/jquery.redactor.fullscreen
